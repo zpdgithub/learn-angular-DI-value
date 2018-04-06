d748db39fb0f395964d84d1b04d896509b557b63
peter <peter@soez.io>
2018-04-06 11:08:49 +0800

    注入ApiService类，注入API_URL值

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { ApiService, API_URL } from './services/ApiService';
 
 @Component({
   selector: 'app-root',
-  template: `root`
+  template: `
+  <button (click)="invokeApi()">Invoke API</button>
+  `
 })
 export class AppComponent {
+  constructor(
+    private apiService: ApiService, // 注入ApiService类
+    // @Inject(ApiService) private apiService: ApiService,  //注入ApiService类的明确写法
+    @Inject(API_URL) private api_url: string  // 注入API_URL值
+  ) {
+  }
+  invokeApi(): void {
+    this.apiService.get();
+    console.log(this.api_url);
+  }
 }
+// 配置
+const isProduction: boolean = true;
 
 @NgModule({
   declarations: [
@@ -16,7 +32,14 @@ export class AppComponent {
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ApiService, // 提供ApiService类
+    { // 提供API_URL值
+      provide: API_URL,
+      useValue: isProduction ? 'https://production-api.sample.com' :
+        'http://dev-api.sample.com'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
